9783055bf30b9c7e1984f4322f56e5f271d1ae6b
kintsugi-warrior <siddhant22496@iiitd.ac.in>
2024-09-02 10:38:54 +0530

    Create Post

--- lifelore/components/Nav.jsx
+++ lifelore/components/Nav.jsx
@@ -7,8 +7,31 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 import React from 'react';
 
 const Nav = () => {
+  const isUserLoggedIn = true;
   return (
-    <div>Nav</div>
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href='/' className='flex gap-2 flex-center'>
+        <Image
+          src='/assets/images/logo.svg'
+          alt='lifelore logo'
+          width={30}
+          height={30}
+          className='object-contain'
+        />
+        <p className='logo_text'>LifeLore</p>
+      </Link>
+
+      {/* Desktop Nav */}
+      <div className='sm:flex hidden'>
+      {isUserLoggedIn ? (
+        <div className='flex gap-3 md:gap-5'>
+          <Link className='black_btn' href="/create-exp" >Create Post</Link>
+        </div>
+        ):(
+          <></>
+        )}
+      </div>
+      </nav>
   );
 }
 
