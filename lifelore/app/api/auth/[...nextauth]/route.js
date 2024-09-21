e0fa088baaa69c30966db78ab2a9e8cefe08773c
Siddhant Bali <119037976+kintsugi-programmer@users.noreply.github.com>
2024-09-22 01:36:37 +0530

    auth

--- lifelore/app/api/auth/[...nextauth]/route.js
+++ lifelore/app/api/auth/[...nextauth]/route.js
@@ -3,3 +3,25 @@
 // here we can use providers like google auth
 // backend endpoints
 // 1;19;00
+
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+
+const handler = NextAuth({
+    providers: [
+      GoogleProvider({
+        clientId: "",
+        clientSecret: ""
+      })
+    ],
+    callbacks: {
+      async session({ session }) {
+      },
+      async signIn({ profile, }) {
+      },
+    }
+  })
+  
+  export { handler as GET, handler as POST }
+  
